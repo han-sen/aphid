5e6dcd228c798a1016d97ee4e139b0b9872f7aee
Michael Hansen <michaelandhansen@gmail.com>
2020-11-22 14:35:13 -0500

    added github tab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,32 @@
 import React from "react";
 import Login from "./Login";
 import HeroSVG from "../aphid-hero.png";
+import GitTab from "./GitTab";
 
 export default function Home() {
     return (
-        <section className="home_wrap">
-            <div className="home_wrap_inner">
-                <div className="home_inner_panel">
-                    <h1 className="home_title">Aphid</h1>
-                    <p>
-                        An open-source bug tracking dashboard for cataloging
-                        your project's code issues.
-                    </p>
-                    <Login />
-                    <a href="/">
-                        <button className="button is-info">
-                            <i className="fas fa-bug"></i>View Demo
-                        </button>
-                    </a>
+        <>
+            <GitTab />
+            <section className="home_wrap">
+                <div className="home_wrap_inner">
+                    <div className="home_inner_panel">
+                        <h1 className="home_title">Aphid</h1>
+                        <p>
+                            An open-source bug tracking dashboard for cataloging
+                            your project's code issues.
+                        </p>
+                        <Login />
+                        <a href="https://han-sen.github.io/aphid-demo/">
+                            <button className="button is-info">
+                                <i className="fas fa-bug"></i>View Demo
+                            </button>
+                        </a>
+                    </div>
+                    <div className="home_inner_panel">
+                        <img src={HeroSVG} alt="hero" className="home_image" />
+                    </div>
                 </div>
-                <div className="home_inner_panel">
-                    <img src={HeroSVG} alt="hero" className="home_image" />
-                </div>
-            </div>
-        </section>
+            </section>
+        </>
     );
 }
